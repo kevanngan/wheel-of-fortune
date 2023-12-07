ee15a83987cb8ea3a130254b3e64f9083b6c1e05
Kevan Ngan <kevanngan_@hotmail.com>
2023-12-07 00:44:20 -0800

    Update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -18,63 +18,14 @@ function startNewGame() {
 
 // Function to display the guess board
 function displayGuessBoard() {
-  const guessBoard = document.getElementById("guess-board");
-  guessBoard.textContent = "";
-  for (let letter of currentWord) {
-    if (letter === ' ') {
-      guessBoard.innerHTML += "&nbsp;";
-    } else {
-      guessBoard.textContent += "_";
-    }
-    guessBoard.textContent += " ";
-  }
-}
-
-// Function to update the cash amount
-function updateCashAmount() {
-  const cashAmountElement = document.getElementById("cash-amount");
-  cashAmountElement.textContent = cashAmount;
-}
-
-// Event listener for spin button
-document.getElementById("spin-btn").addEventListener("click", () => {
-
-  const spinResult = Math.floor(Math.random() * 901) + 100;
-  cashAmount += spinResult;
-});
-
-// Event listener for guess letter button
-document.getElementById("guess-letter-btn").addEventListener("click", () => {
-   
-    const letterInput = document.getElementById("letter-input");
-    const guessedLetter = letterInput.value.toLowerCase();
-    
-    if (guessedLetter.length === 1 && currentWord.includes(guessedLetter)) {
-      for (let i = 0; i < currentWord.length; i++) {
-        if (currentWord[i] === guessedLetter) {
-    
-          const guessBoard = document.getElementById("guess-board");
-          const guessBoardContent = guessBoard.textContent.split(" ");
-          guessBoardContent[i] = guessedLetter;
-          guessBoard.textContent = guessBoardContent.join(" ");
-        }
+    const guessBoard = document.getElementById("guess-board");
+    guessBoard.textContent = "";
+    for (let letter of currentWord) {
+      if (letter === ' ') {
+        guessBoard.innerHTML += "&nbsp;";
+      } else {
+        guessBoard.textContent += "_";
       }
-
-      cashAmount += 200; 
-      updateCashAmount();
+      guessBoard.textContent += " ";
     }
-
-    letterInput.value = "";
-});
-
-// Event listener for guess word button
-document.getElementById("guess-word-btn").addEventListener("click", () => {
-  const guessedWord = prompt("Enter the word").toLowerCase();
-  if (guessedWord === currentWord) {
-    cashAmount += 500; 
-    updateCashAmount();
   }
-});
-
-// Start a new game
-startNewGame();
