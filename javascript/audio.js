04e8ddc644e837170ae156bfbc1b9d526e2d9438
Kevan Ngan <kevanngan_@hotmail.com>
2023-12-15 15:58:25 -0800

    Add sound effects for winning or losing

--- javascript/audio.js
+++ javascript/audio.js
@@ -3,12 +3,15 @@
 // Audio
 
   // Background music constructor
-  function BackgroundMusic(src) {
+function BackgroundMusic(src) {
     this.audio = new Audio(src);
+    this.correctSound = new Audio('/audio/correct.mp3'); // Change the path accordingly
+    this.wrongSound = new Audio('/audio/wrong.mp3'); // Change the path accordingly
+  
     this.audio.volume = 0.5; // Set default volume
-    this.play = function() {
+    this.play = function () {
       const playPromise = this.audio.play();
-
+  
       if (playPromise !== undefined) {
         playPromise.catch(error => {
           // Autoplay was prevented
@@ -17,36 +20,65 @@
         });
       }
     };
-    this.pause = function() {
+    this.pause = function () {
       this.audio.pause();
     };
-    this.toggleVolume = function() {
+    this.toggleVolume = function () {
       this.audio.volume = this.audio.volume === 0 ? 0.5 : 0;
+      updateVolumeIcon();
     };
-
+  
+    this.playCorrectSound = function () {
+      this.correctSound.play();
+    };
+  
+    this.playWrongSound = function () {
+      this.wrongSound.play();
+    };
+  
     // Repeat the music when it ends
-    this.audio.addEventListener('ended', function() {
+    this.audio.addEventListener('ended', function () {
       this.currentTime = 0; // Reset to the beginning
       this.play();
     });
   }
-
+  
   // Create an instance of BackgroundMusic
-  const gameMusic = new BackgroundMusic('/audio/background.music.mp3');
-
+  const gameMusic = new BackgroundMusic('/audio/background-music.mp3');
+  
   // Function to toggle volume when clicking the volume icon
   function toggleVolume() {
     gameMusic.toggleVolume();
-    // Update the volume icon based on the current volume
-    const volumeIcon = document.getElementById('volume-icon');
-    volumeIcon.textContent = gameMusic.audio.volume === 0 ? '🔈' : '🔊';
+    updateVolumeIcon();
   }
-
+  
+  // Function to update the volume icon based on the current volume
+  function updateVolumeIcon() {
+    const volumeIcon = document.getElementById('volume-icon');
+    const iconClass = gameMusic.audio.volume === 0 ? 'fa-volume-mute' : 'fa-volume-up';
+    volumeIcon.innerHTML = `<i class="fas ${iconClass}"></i>`;
+  }
+  
+  // Function to handle correct guess
+  function handleCorrectGuess() {
+    // Play correct sound
+    gameMusic.playCorrectSound();
+    // Add any additional logic for correct guess
+  }
+  
+  // Function to handle incorrect guess
+  function handleIncorrectGuess() {
+    // Play wrong sound
+    gameMusic.playWrongSound();
+    // Add any additional logic for incorrect guess
+  }
+  
   // Function to start the game on any user click
   function startGame() {
     document.body.removeEventListener('click', startGame); // Remove the event listener
     gameMusic.play();
   }
-
+  
   // Add an event listener to start the game on any user click
   document.body.addEventListener('click', startGame);
+
